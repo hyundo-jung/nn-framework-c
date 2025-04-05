3a5fe6dfb936e84dc6e8c1a61b21cf8e7d7423fc
Hyundo Jung <hyundo-jung@users.noreply.github.com>
2025-04-04 23:11:15 -0500

    implemented sum, dot

--- nn.c
+++ nn.c
@@ -1,10 +1,23 @@
 #define NN_IMPLEMENTATION
 #include "nn.h"
 #include <stdio.h>
+#include <time.h>
 
 int main(void)
 {
-    Mat m = mat_alloc(3, 1);
-    mat_print(m);
+    srand(time(0));
+    Mat a = mat_alloc(2, 3);
+    mat_fill(a, 2);
+    mat_print(a);
+
+    Mat b = mat_alloc(3, 2);
+    mat_fill(b, 5);
+    mat_print(b);
+
+    Mat dst = mat_alloc(2, 2);
+
+    printf("--------------\n");
+    mat_dot(dst, a, b);
+    mat_print(dst);
     return 0;
 }
--- nn.h
+++ nn.h
@@ -22,8 +22,11 @@ typedef struct {
 
 #define MAT_AT(m, i, j) m.es[(i)*m.cols + (j)]
 
+float rand_float(void);
+
 Mat mat_alloc(size_t rows, size_t cols);
-void mat_rand(Mat m);
+void mat_fill(Mat m, float x);
+void mat_rand(Mat m, float low, float high);
 void mat_dot(Mat dst, Mat a, Mat b);
 void mat_sum(Mat dst, Mat a);
 void mat_print(Mat m);
@@ -31,6 +34,11 @@ void mat_print(Mat m);
 #endif //NN_H_
 
 #ifdef NN_IMPLEMENTATION
+
+float rand_float(void)
+{
+    return (float) rand() / (float) RAND_MAX;
+}
 
 Mat mat_alloc(size_t rows, size_t cols)
 {
@@ -42,19 +50,64 @@ Mat mat_alloc(size_t rows, size_t cols)
     return m;
 }
 
-void mat_rand(Mat m);
+void mat_fill(Mat m, float x)
+{
+    for (size_t i = 0; i < m.rows; i++)
+    {
+        for (size_t j = 0; j < m.cols; j++)
+        {
+            MAT_AT(m, i, j) = x;
+        }
+    }
+}
+
+void mat_rand(Mat m, float low, float high)
+{
+    for (size_t i = 0; i < m.rows; i++)
+    {
+        for (size_t j = 0; j < m.cols; j++)
+        {
+            MAT_AT(m, i, j) = rand_float()*(high - low) + low;
+        }
+    }
+}
 
 void mat_dot(Mat dst, Mat a, Mat b)
 {
+    NN_ASSERT(a.cols == b.rows); // inner matrix
+    size_t n = a.cols;
+    NN_ASSERT(dst.rows == a.rows); // outer matrix
+    NN_ASSERT(dst.cols == b.cols);
+
+    for (size_t i = 0; i < dst.rows; i++)
+    {
+        for (size_t j = 0; j < dst.cols; j++)
+        {
+            MAT_AT(dst, i, j) = 0;
+            for (size_t k = 0; k < n; k++)
+            {
+                MAT_AT(dst, i, j) += MAT_AT(a, i, k) * MAT_AT(b, k, j);    
+            }  
+        }
+    }
+
     return;
 }
 
 void mat_sum(Mat dst, Mat a)
 {   
+    NN_ASSERT(dst.rows == a.rows);
+    NN_ASSERT(dst.cols == a.cols);
+
+    for (size_t i = 0; i < dst.rows; i++)
+    {
+        for (size_t j = 0; j < dst.cols; j++)
+        {
+            MAT_AT(dst, i, j) += MAT_AT(a, i, j);
+        }
+    }
     return;
 }
-
-
 
 void mat_print(Mat m)
 {
