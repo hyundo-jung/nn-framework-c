e5549b05509d4f35e4c937bb9dd26d1b5edfa8f6
Hyundo Jung <hyundo-jung@users.noreply.github.com>
2025-04-06 16:08:45 -0500

    added forwarding, cost calculation

--- nn.c
+++ nn.c
@@ -1,23 +1,121 @@
 #define NN_IMPLEMENTATION
 #include "nn.h"
-#include <stdio.h>
 #include <time.h>
+
+typedef struct {
+    Mat a0;
+
+    Mat w1, b1, a1;
+    Mat w2, b2, a2;
+
+} Xor;
+
+float forward_xor(Xor m)
+{
+    mat_dot(m.a1, m.a0, m.w1);
+    mat_sum(m.a1, m.b1);
+    mat_sig(m.a1);
+
+    mat_dot(m.a2, m.a1, m.w2);
+    mat_sum(m.a2, m.b2);
+    mat_sig(m.a2);
+
+    return *m.a2.es;
+}
+
+float cost(Xor m, Mat ti, Mat to)
+{
+    assert(ti.rows == to.rows);
+    assert(to.cols == m.a2.cols);
+    size_t n = ti.rows;
+    size_t q = to.cols;
+
+    float c = 0.0f;
+    for (size_t i = 0; i < n; i++)
+    {
+        // MAT_AT(m.a0, 0, 0) = MAT_AT(ti, n, 0);
+        // MAT_AT(m.a0, 0, 0) = MAT_AT(ti, n, 1);
+        Mat x = mat_row(ti, i);
+        Mat y = mat_row(to, i);
+
+        mat_copy(m.a0, x);
+        forward_xor(m);
+
+        for (size_t j = 0; j < q; j++)
+        {
+            float d = MAT_AT(m.a2, i, j) - MAT_AT(y, i, j);
+            c += d*d;
+        }
+    }
+    c /= n;
+    return c;
+}
+
+float td[][3] = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9},
+    {10, 11, 12},
+};
+
 
 int main(void)
 {
     srand(time(0));
-    Mat a = mat_alloc(2, 3);
-    mat_fill(a, 2);
-    mat_print(a);
 
-    Mat b = mat_alloc(3, 2);
-    mat_fill(b, 5);
-    mat_print(b);
+    size_t stride = 3;
+    size_t n = sizeof(td) / sizeof(td[0]);
+    Mat ti = {
+        .rows = n,
+        .cols = 2,
+        .stride = stride,
+        .es = td[0],
+    };
 
-    Mat dst = mat_alloc(2, 2);
+    Mat to = {
+        .rows = n,
+        .cols = 1,
+        .stride = stride,
+        .es = td[0] + 2,
+    };
 
-    printf("--------------\n");
-    mat_dot(dst, a, b);
-    mat_print(dst);
+    MAT_PRINT(ti);
+    MAT_PRINT(to);
+
+    Xor m;
+    m.a0 = mat_alloc(1, 2);
+
+    // first layer of XOR
+    m.w1 = mat_alloc(2, 2);
+    m.b1 = mat_alloc(1, 2);
+
+    // second layer
+    m.w2 = mat_alloc(2, 1);
+    m.b2 = mat_alloc(1, 1);
+
+    // activation
+    m.a1 = mat_alloc(1, 2);
+    m.a2 = mat_alloc(1, 1);
+
+    mat_rand(m.w1, 0, 1);
+    mat_rand(m.b1, 0, 1);
+    mat_rand(m.w2, 0, 1);
+    mat_rand(m.b2, 0, 1);  
+
+    printf("cost: %f \n", cost(m, ti, to));
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        for (size_t j = 0; j < 2; j++)
+        {
+            MAT_AT(m.a0, 0, 0) = i;
+            MAT_AT(m.a0, 0, 1) = j;
+            forward_xor(m);
+            float y = *(m.a2.es);
+
+            printf("%zu ^ %zu = %f\n", i, j, y);
+        }
+    }
+  
     return 0;
 }
--- nn.h
+++ nn.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <math.h>
 
 #ifndef NN_MALLOC
 #include <stdlib.h>
@@ -17,19 +18,24 @@
 typedef struct {
     size_t rows;
     size_t cols;
+    size_t stride;
     float* es;
 } Mat;
 
-#define MAT_AT(m, i, j) m.es[(i)*m.cols + (j)]
+#define MAT_AT(m, i, j) m.es[(i)*m.stride + (j)]
 
 float rand_float(void);
-
+float sigmoidf(float x);
 Mat mat_alloc(size_t rows, size_t cols);
 void mat_fill(Mat m, float x);
 void mat_rand(Mat m, float low, float high);
+Mat mat_row(Mat m, size_t row);
+void mat_copy(Mat dst, Mat src);
 void mat_dot(Mat dst, Mat a, Mat b);
 void mat_sum(Mat dst, Mat a);
-void mat_print(Mat m);
+void mat_sig(Mat m);
+void mat_print(Mat m, char* name);
+#define MAT_PRINT(m) mat_print(m, #m)  
 
 #endif //NN_H_
 
@@ -40,11 +46,17 @@ float rand_float(void)
     return (float) rand() / (float) RAND_MAX;
 }
 
+float sigmoidf(float x)
+{
+    return 1.f / (1.f + expf(-x));
+}
+
 Mat mat_alloc(size_t rows, size_t cols)
 {
     Mat m;
     m.rows = rows;
     m.cols = cols;
+    m.stride = cols;
     m.es = NN_MALLOC(sizeof(*m.es) * rows * cols);
     NN_ASSERT(m.es != NULL);
     return m;
@@ -68,6 +80,30 @@ void mat_rand(Mat m, float low, float high)
         for (size_t j = 0; j < m.cols; j++)
         {
             MAT_AT(m, i, j) = rand_float()*(high - low) + low;
+        }
+    }
+}
+
+Mat mat_row(Mat m, size_t row)
+{
+    return (Mat) {
+        .rows = 1,
+        .cols = m.cols,
+        .stride = m.stride,
+        .es = &MAT_AT(m, row, 0), // beginning of specific row
+    };
+}
+
+void mat_copy(Mat dst, Mat src)
+{
+    NN_ASSERT(dst.rows == src.rows);
+    NN_ASSERT(dst.cols == src.cols);
+
+    for (size_t i = 0; i < src.rows; i++)
+    {
+        for (size_t j = 0; j < src.cols; j++)
+        {
+            MAT_AT(dst, i, j) = MAT_AT(src, i, j);
         }
     }
 }
@@ -109,16 +145,29 @@ void mat_sum(Mat dst, Mat a)
     return;
 }
 
-void mat_print(Mat m)
+void mat_sig(Mat m)
 {
     for (size_t i = 0; i < m.rows; i++)
     {
         for (size_t j = 0; j < m.cols; j++)
         {
-            printf("%f ", MAT_AT(m, i, j));
+            MAT_AT(m, i, j) = sigmoidf(MAT_AT(m, i, j));
+        }
+    }
+}
+
+void mat_print(Mat m, char* name)
+{
+    printf("%s = [\n", name);
+    for (size_t i = 0; i < m.rows; i++)
+    {
+        for (size_t j = 0; j < m.cols; j++)
+        {
+            printf("    %f ", MAT_AT(m, i, j));
         }
         printf("\n");
     }
+    printf("]\n");
 }
 
 #endif // NN_IMPLEMENTATION
